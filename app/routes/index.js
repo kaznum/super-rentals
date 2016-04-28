c57c8ef3ab43db7fb6bd16066a9d84332eb4fe28
Kazuya NUMATA <numata@gmail.com>
2016-04-28 18:27:32 +0900

    Implement index.hbs and its route handler

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,34 @@
 import Ember from 'ember';
 
+var rentals = [{
+  id: 1,
+  title: 'Sapporo Station Tower',
+  owner: 'Yamada Taro',
+  city: 'Sapporo',
+  type: 'Estate',
+  bedrooms: 13,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+}, {
+  id: 2,
+  title: 'Urban Living',
+  owner: 'Mike TV',
+  city: 'Seattle',
+  type: 'Condo',
+  bedrooms: 1,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+}, {
+  id: 3,
+  title: 'Downtown Charm',
+  owner: 'Violet Beauregarde',
+  city: 'Portland',
+  type: 'Apartment',
+  bedrooms: 3,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+}];
+
+
 export default Ember.Route.extend({
+  model() {
+    return rentals;
+  }
 });
